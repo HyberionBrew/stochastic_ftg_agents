79d1894e64bc6074e2b831a97d33886157845803
Fabian Kresse <e11707724@student.tuwien.ac.at>
2023-12-04 16:28:34 +0100

    fixed the agent

--- ftg_agents/agents_numpy.py
+++ ftg_agents/agents_numpy.py
@@ -38,35 +38,54 @@ class BaseAgent(object):
 @input lidar rays need to be already normalized
 """
 class StochasticContinousFTGAgent(BaseAgent):
-    def __init__(self, deterministic=False, horizon=0.2, subsample=20,gap_size_max_speed=10):
+    def __init__(self, current_speed=0.0, deterministic=False, horizon=0.2, subsample=20,gap_size_max_speed=10):
         # initalize parent
         super(StochasticContinousFTGAgent, self).__init__()
         self.deterministic = deterministic
         self.horizon = horizon 
         self.subsample = 20
         self.current_angle = 0.0
-        self.current_velocity = 0.0
+        self.current_velocity = current_speed
         self.max_change = 0.05
         self.exp_decay = 0.1
         self.gap_max = gap_size_max_speed
-        self.gap_blocker = 3
+        self.gap_blocker = 2
+        self.start_velocity = current_speed
+        self.speed_multiplier = 2.0 # higher values slower agent
 
     def get_delta_angle(self, target, current):
+        #print("delta")
         angle_diff = target - current
+        #print(angle_diff)
         delta_angle = np.sign(angle_diff) * min(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/abs(angle_diff)))
         new_angle = current + delta_angle
+        #print(new_angle)
         new_angle = np.clip(new_angle, -3*np.pi/4, 3*np.pi/4)
         return delta_angle, new_angle
+    
     def compute_speed(self, gap):
         gap_size = len(gap)
         # if gap size is 10 full speed else, linear
         gap_size = min(gap_size, self.gap_max)
-        speed = 0.5 + (gap_size / 10.0) * 1.5
+        #print("gap size", gap_size)
+        # max ray in gap 
+        max_ray = max(gap)
+        # clip max ray between 0 and 10
+        max_ray = np.clip(max_ray, 0.0, self.speed_multiplier)
+        speed = 0.5 + (max_ray / self.speed_multiplier) * 1.5
         return speed
     
+    def reset(self):
+        self.current_angle = 0.0
+        self.current_velocity = self.start_velocity
+
     def get_delta_speed(self, target, current):
         speed_diff = target - current
-        delta_speed = np.sign(speed_diff) * min(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/abs(speed_diff)))
+        if np.abs(speed_diff) > 0.001:
+            #print(speed_diff)
+            delta_speed = np.sign(speed_diff) * min(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/abs(speed_diff)))
+        else:
+            delta_speed = 0.0
         new_speed = current + delta_speed
         new_speed = np.clip(new_speed, 0.5, 2.0)
         return delta_speed, new_speed
@@ -76,8 +95,19 @@ class StochasticContinousFTGAgent(BaseAgent):
         # input can be a torch tensor or a numpy array
         scans = model_input_dict['lidar_occupancy'][0].copy()
         assert scans.ndim == 1, "Scans should be a 1D array"
-        
-        scans[scans < self.horizon] = -1.0
+        horizon = self.horizon
+        while horizon>0.1:
+            scans_temp = scans.copy()
+            scans_temp[scans < horizon] = -1.0
+            if np.all(scans_temp < 0.0):
+                horizon -= 0.05
+            else:
+                break
+        # block 10% of the left and rightmost scans
+        scans_temp[:int(len(scans_temp)*0.15)] = -1.0
+        scans_temp[-int(len(scans_temp)*0.15):] = -1.0
+
+        scans = scans_temp
         negative_indices = np.where(scans < 0.0)[0]
         for index in negative_indices:
             start_index = max(0, index - self.gap_blocker)
@@ -85,10 +115,15 @@ class StochasticContinousFTGAgent(BaseAgent):
             scans[start_index:end_index] = -1.0
         # now lets find the index of the max
         max_index = np.argmax(scans)
+        #print(max_index)
         max_value = scans[max_index]
+        #if max_value < 0:
+        #print("MAX VALUE @ and horizon", max_value, max_index)
+        #print(scans)
         # find the window of non-negative values around the target index
         # find the first non-negative value to the left
         # padd scans with -1.0 left and right
+        
         left_scans = scans[max_index::-1]
         # find first negative
 
@@ -107,9 +142,12 @@ class StochasticContinousFTGAgent(BaseAgent):
             first_right = len(scans) -1
         else:
             first_right = min(max_index + first_right[0][0], len(scans)-1)
+        #print("first left and right")
+        #print(first_left, first_right)
 
         target_ray = int((first_left + first_right) // 2) 
-
+        #print("target ray")
+        #print(target_ray)
         if max_value < 0:
             target_angle = 0.0
         else:
@@ -117,23 +155,79 @@ class StochasticContinousFTGAgent(BaseAgent):
         #print(target_angle)
         # now we need to implement delta change
         target_angle = target_angle #- self.current_angle
+        # target_angle = 0.5
+        #print(self.current_angle)
+        #print(target_angle)
+        #print("------")
         # clip target angle between -3/4 pi and 3/4 pi
         target_angle = np.clip(target_angle, -3*np.pi/4, 3*np.pi/4)
         target_speed = self.compute_speed(scans[first_left:first_right])
         delta_angle, current_angle = self.get_delta_angle(target_angle, self.current_angle)
         delta_speed, current_velocity = self.get_delta_speed(target_speed, self.current_velocity)
-        self.current_angle = current_angle
+        self.current_angle = current_angle # * -1.0
         self.current_velocity = current_velocity
-        return np.array([[delta_speed, delta_angle]])
-    
+        print(self.current_velocity)
+        return np.array([[delta_angle,delta_speed]])
+
+eval_config = {
+    "collision_penalty": -10.0,
+    "progress_weight": 1.0,
+    "raceline_delta_weight": 0.0,
+    "velocity_weight": 0.0,
+    "steering_change_weight": 0.0,
+    "velocity_change_weight": 0.0,
+    "pure_progress_weight": 0.0,
+    "inital_velocity": 1.5,
+    "normalize": False,
+}
+
 if __name__ == "__main__":
-    agent = StochasticContinousFTGAgent()
+    from f110_sim_env.base_env import make_base_env
+    agent = StochasticContinousFTGAgent(current_speed = 0.5)
     rays = np.ones((1,54,)) * 0.19
     rays[:,:10] += 0.1
     #rays += 0.5
     #print(rays)
     model_input_dict = {'lidar_occupancy': rays}
     #print(model_input_dict)
-    for _ in range(40):
-        target = agent(model_input_dict)
-        print(target)
+    #for _ in range(40):
+    #    target = agent(model_input_dict)
+    #    print(target)
+
+    eval_env = make_base_env(map= "Infsaal",
+            fixed_speed=None,
+            random_start =True,
+            train_random_start = False,
+            reward_config = eval_config,
+            eval=False,
+            use_org_reward=True,
+            min_vel=0.5,
+            max_vel=0.5,
+        )
+
+    action = np.array([0.0,0.0])
+    i = 0
+    while i < 6:
+        eval_env.reset()
+        agent.reset()
+        i += 1
+        done = False
+        truncated = False
+        j = 0
+        while not done and not truncated:
+            obs, reward, done, truncated, info = eval_env.step(action)
+            #print(info["observations"]["lidar_occupancy"])
+            #print("prev action")
+            #print(info["observations"]["previous_action"])
+            obs_dict = {'lidar_occupancy':  np.array([info["observations"]["lidar_occupancy"]])}
+            action = agent(model_input_dict=obs_dict)
+            # rescale action to be between -1 and 1, 0.05 is one
+            action = action/0.05
+
+            #print("curr action")
+            #print(action)
+            eval_env.render()
+            if j == 3:
+                pass
+                #exit()
+            j += 1
