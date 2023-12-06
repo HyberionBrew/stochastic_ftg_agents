04506cc91b875444582347d4f64466a4fb49c5c1
Fabian Kresse <e11707724@student.tuwien.ac.at>
2023-12-06 16:09:54 +0100

    updated agent for parallelized actions

--- ftg_agents/agents_numpy.py
+++ ftg_agents/agents_numpy.py
@@ -58,19 +58,19 @@ class StochasticContinousFTGAgent(BaseAgent):
 
     def get_delta_angle(self, target, current):
         #print("delta")
-        angle_diff = target - current
-        if np.abs(angle_diff) < 0.001:
-            #print(angle_diff)
-            delta_angle = 0.0
-            return delta_angle, current
+        angle_diff = target - current[:None]
+        angle_diff[np.isclose(angle_diff,0.0)] = 0.0001 
         #print(angle_diff)
-        delta_angle = np.sign(angle_diff) * min(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/abs(angle_diff)))
+        #print(self.max_change)
+        #print(np.array(self.max_change))
+        delta_angle = np.sign(angle_diff) * np.minimum(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/np.abs(angle_diff)))
         new_angle = current + delta_angle
         #print(new_angle)
         new_angle = np.clip(new_angle, -3*np.pi/4, 3*np.pi/4)
         return delta_angle, new_angle
     
     def compute_speed(self, gap):
+        assert gap.ndim == 1, "Gap should be a 1D array"
         """
         gap_size = len(gap)
         if len(gap)==0:
@@ -92,7 +92,7 @@ class StochasticContinousFTGAgent(BaseAgent):
         print(max_ray)
         print("------")
         """
-        max_ray = gap[0]
+        max_ray = gap  #[0] # TODO gap is a value of shape (batch, 1)
         max_ray = np.clip(max_ray, 0.0, self.speed_multiplier)
         speed = 0.0 + (max_ray / self.speed_multiplier) * self.max_speed
         # print(max_ray)
@@ -104,11 +104,10 @@ class StochasticContinousFTGAgent(BaseAgent):
 
     def get_delta_speed(self, target, current):
         speed_diff = target - current
-        if np.abs(speed_diff) > 0.001:
+        speed_diff[np.isclose(speed_diff,0.0)] = 0.0001 
             #print(speed_diff)
-            delta_speed = np.sign(speed_diff) * min(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/abs(speed_diff)))
-        else:
-            delta_speed = 0.0
+        delta_speed = np.sign(speed_diff) * np.minimum(self.max_change, self.max_change * np.exp(-self.exp_decay * 1/abs(speed_diff)))
+
         new_speed = current + delta_speed
         new_speed = np.clip(new_speed, self.start_velocity, 2.0)
         return delta_speed, new_speed
@@ -116,7 +115,118 @@ class StochasticContinousFTGAgent(BaseAgent):
     def __str__(self):
         return f"StochasticContinousFTGAgent_{self.speed_multiplier}_{self.gap_blocker}_{self.horizon}_{self.std}_{self.max_speed}"
     
-    def compute_target(self, scans):
+    def set_zeros_around_max(self,scans_processed, max_indices):
+        batch_size, num_scans = scans_processed.shape
+
+        # Identify where the -1.0 separators are
+        separators = scans_processed == -1.0
+
+        # Create a cumulative sum mask to identify contiguous regions
+        cumsum_mask = np.cumsum(separators, axis=1)
+
+        # For each batch, find the cumsum value at the max_index
+        regions = cumsum_mask[np.arange(batch_size), max_indices]
+
+        # Create masks for the contiguous regions containing the max_index
+        contiguous_region_mask = (cumsum_mask == regions[:, None])
+
+        # Zero out elements not in the contiguous region of max_index
+        result = np.where(contiguous_region_mask, scans_processed, 0.0)
+        # set the first non zero element in each row to 0.0
+        # above code has small bug and leaves first -1.0 in conitnous result region
+        # easy fix ~.+
+        result = np.where(result == -1.0, 0, result)
+
+        return result
+
+
+    def compute_target(self, scans_batch):
+        assert scans_batch.ndim == 2, "Scans should be a 2D array with shape (batches, dim)"
+        horizon = self.horizon
+        og_scans = scans_batch.copy()
+        # block 15% of the left and rightmost scans
+        block_length = int(len(scans_batch[0])*0.15)
+        scans_batch[:,:block_length] = -1.0
+        scans_batch[:,-block_length:] = -1.0
+        # now lets iterate over the horizon
+        #scans_finished_indices = np.zeros(len(scans_batch))
+        scans_done = np.zeros(len(scans_batch))
+        scans_processed = np.zeros_like(scans_batch) - 1.0
+
+        while horizon > 0.05:
+            #print(horizon)
+            #print("done:", scans_done)
+            scans_temp = scans_batch.copy()
+            scans_missing_indices = np.where(scans_done == 0.0)[0]
+
+            current_scan = scans_temp[scans_missing_indices] #= -1.0
+            current_scan[current_scan < horizon] = -1.0
+
+            # check each row if all are negative
+            # block left and rights
+            # get the index of all negatives
+            negatives_batch, negatives_dim = np.where(current_scan < 0.0)
+            # block out the gaps
+            for i in range(1, self.gap_blocker + 1):
+                negatives_dim_left = negatives_dim - i
+                negatives_dim_right = negatives_dim + i
+                # ensure we dont go out of bounds
+                negatives_dim_left = np.clip(negatives_dim_left, 0, len(current_scan[0])-1)
+                negatives_dim_right = np.clip(negatives_dim_right, 0, len(current_scan[0])-1)
+
+                current_scan[negatives_batch, negatives_dim_left] = -1.0
+                current_scan[negatives_batch, negatives_dim_right] = -1.0
+            # check if all are negative
+
+            # print(negatives)
+            positives = np.any(current_scan > 0.0, axis=1)
+            # where we have positives we write into scans_processed
+            scans_processed[scans_missing_indices[positives]] = current_scan[positives]
+            scans_done[scans_missing_indices[positives]] = 1.0
+            #print(scans_done)
+            #print(scans_processed)
+            
+            if np.any(scans_done == 0.0):
+                horizon -= 0.05
+            else:
+                break
+
+        # now all scans are processed
+        # now lets find the index of the max
+        max_indices = np.argmax(scans_processed, axis=1)
+        # where the max is negative we set the index to scans.shape[1]//2
+        #kinda hacky but implicitly true ~.~
+        # where max_indices are 0 we set scans_processed to 0.5
+        scans_processed[max_indices == 0, len(scans_processed[0])//2] = 0.5
+        max_indices[max_indices == 0] = len(scans_processed[0])//2
+
+        #print(max_indices)
+        # for each find the left and rightmost non zero
+        # left
+
+        #right_scans = scans_processed[max_indices::]
+        #left_scans = scans_processed[max_indices::-1]
+        # find first negative
+        
+        #first_left = np.argwhere(left_scans < 0.0, axis=1)
+        #first_right = np.argwhere(right_scans < 0.0,axis=1)
+        new_scans = self.set_zeros_around_max(scans_processed, max_indices)
+        #print(new_scans)
+        # in each row find first and last non zero element
+        left_indices = np.where(new_scans > 0.0)#[0]
+        scans = new_scans.copy()
+        scans[scans > 0] = 1.0
+        left_indices = np.argmax(scans, axis=1)
+
+        right_indices = np.argmax(scans[:,::-1], axis=1)
+        right_indices = len(scans[0]) - right_indices - 1
+        target_rays = (left_indices + right_indices) // 2
+
+        target_angle = self.lidar_ray_to_steering(target_rays, self.subsample)
+        target_speed = self.compute_speed(og_scans[:,og_scans.shape[1]//2])
+        return target_angle, target_speed
+
+    def compute_target_old(self, scans):
         # print(scans)
         assert scans.ndim == 1, "Scans should be a 1D array"
         horizon = self.horizon
@@ -175,60 +285,57 @@ class StochasticContinousFTGAgent(BaseAgent):
     
     # use the prev action from the model_input_dict to make this stateless
     def __call__(self, model_input_dict, action=None, std=None):
-        # from the model_input_dict extract the lidar_occupancy
-        # input can be a torch tensor or a numpy array
+        std_angle = self.std
+        std_speed = self.std
+
+        # Asserting the shape of action if it's not None
         if action is not None:
-            assert action.ndim == 1, "Action should be a 1D array"  
-        #if not(self.deterministic):
-        # TODO! clean this up
-        std_angle = self.std #if std_angle is None else std_angle
-        std_speed = self.std #if std_speed is None else std_speed
-        scans = model_input_dict['lidar_occupancy'].copy()
-        prev_action = model_input_dict['previous_action'][0].copy()
-        current_angle = prev_action[0]
-        current_velocity = prev_action[1]
-        
-        target_angle, target_speed = self.compute_target(scans)
-        #print(target_angle)
-        # now we need to implement delta change
-        # clip target angle between -3/4 pi and 3/4 pi
-        
-        # if the gap is not existent, then make a fake gap that is in the middle
-        
-        delta_angle, current_angle = self.get_delta_angle(target_angle, current_angle)
-        delta_speed, current_velocity = self.get_delta_speed(target_speed, current_velocity)
-        
-        means = np.array([delta_angle,delta_speed]) / 0.05 
-        # clip means to be between -1 and 1
-        lower, upper = -1.0, 1.0
+            assert action.ndim == 2 and action.shape[1] == 2, "Action should be a 2D array with shape (batches, 2)"
+        assert "lidar_occupancy" in model_input_dict
+        assert len(model_input_dict["lidar_occupancy"].shape) == 2, "Lidar occupancy should be a 2D array"
+        assert "previous_action" in model_input_dict
+        assert len(model_input_dict["previous_action"].shape) == 3, "Previous action should be a 3D (batch,1,2) array, yes weird TODO!"
+        scans = model_input_dict['lidar_occupancy']
+        prev_actions = model_input_dict['previous_action']
+        current_angles = prev_actions[:,0, 0]
+        #print(prev_actions.shape)
+        #print(prev_actions[:10])
+        current_velocities = prev_actions[:,0, 1]
 
-        # Adjust means and std if necessary
-        means = np.clip(means, lower, upper)
+        target_angles, target_speeds = self.compute_target(scans)  # Adapted for batch processing
+        #print(target_angles)
+        #print(target_speeds)
+        delta_angles = target_angles - current_angles
+        delta_speeds = target_speeds - current_velocities 
+        # TODO! for now
+        delta_angles, new_current_angles = self.get_delta_angle(target_angles, current_angles)  # Adapted for batch processing
+        delta_speeds, new_current_velocities = self.get_delta_speed(target_speeds, current_velocities)  # Adapted for batch processing
 
-        # Scale the bounds for the truncated normal distribution
-        a, b = (lower - means) / std_angle, (upper - means) / std_angle
+        means = np.vstack((delta_angles, delta_speeds)).T / 0.05
+        means = np.clip(means, -1.0, 1.0)
+
+        a = (means - 1.0) / std_angle
+        b = (means + 1.0) / std_angle
 
         if not self.deterministic:
-            
             targets = np.random.normal(means, std_angle)
         else:
             targets = means
 
-        targets = np.clip(targets, lower, upper)
-        # clip targets to be between
+        targets = np.clip(targets, -1.0, 1.0)
+
+        dist = truncnorm(a, b, loc=means, scale=std_angle)
+
         if action is not None:
-            assert (action <= upper).all() and (action >= lower).all(), f"Action should be between -1 and 1 {action}"
-            # Create a truncated normal distribution object
-            dist = truncnorm(a, b, loc=means, scale=std_angle)
-            log_prob = dist.logpdf(action)
-
+            assert (action <= 1.0).all() and (action >= -1.0).all(), "Action should be between -1 and 1"
+            log_probs = dist.logpdf(action).sum(axis=1)
         else:
-            # Similarly for targets
-            dist = truncnorm(a, b, loc=means, scale=std_angle)
-            log_prob = dist.logpdf(targets)
+            log_probs = dist.logpdf(targets).sum(axis=1)
 
-        # TODO! make stochastic and calculate log prob
-        return  None, targets , np.sum(log_prob, axis=-1)
+        # log_probs = 1.0
+        #print("mean", means)
+        #print("target", targets)
+        return None, targets, log_probs
 
 eval_config = {
     "collision_penalty": -10.0,
@@ -244,8 +351,8 @@ eval_config = {
 
 if __name__ == "__main__":
     from f110_sim_env.base_env import make_base_env
-    agent = StochasticContinousFTGAgent(current_speed = 0.0, deterministic=True)
-    rays = np.ones((1,54,)) * 0.19
+    agent = StochasticContinousFTGAgent(current_speed = 0.0, deterministic=False, std=0.1, speed_multiplier=0.5)
+    rays = np.ones((1,54,)) * 0.1
     rays[:,:10] += 0.1
     #rays += 0.5
     #print(rays)
@@ -264,9 +371,11 @@ if __name__ == "__main__":
             use_org_reward=True,
             min_vel=0.0,
             max_vel=0.0,
+            max_delta_steering=0.05,
+            max_acceleration=0.05,
         )
 
-    action = np.array([0.0,0.0])
+    action = np.array([[0.0,0.0],[0.0,0.0],[0.0,0.0]])
     i = 0
     while i < 6:
         eval_env.reset()
@@ -276,14 +385,24 @@ if __name__ == "__main__":
         truncated = False
         j = 0
         while not done and not truncated:
-            obs, reward, done, truncated, info = eval_env.step(action)
-
-            obs_dict = {'lidar_occupancy':  np.array([info["observations"]["lidar_occupancy"]])}
+            obs, reward, done, truncated, info = eval_env.step(action[2])
+            fake_lidar2 = np.zeros((1,54,))
+            fake_lidar = np.ones((1,54,)) * 0.1
+            fake_lidar[:,26] = 0.
+            fake_lidar[:,30] = 1.0
+            lidar = np.array([info["observations"]["lidar_occupancy"]])
+            # concatenate along dim 0 fake  and real lidar
+            fake_lidar = np.concatenate((fake_lidar2, fake_lidar,lidar), axis=0)
+            #print(fake_lidar.shape)
+            obs_dict = {'lidar_occupancy':  fake_lidar}#,info["observations"]["lidar_occupancy"]])}
             obs_dict['previous_action'] = np.array(info["observations"]["previous_action"])
-            action, _ , log_prob = agent(model_input_dict=obs_dict)
+            _, action , log_prob = agent(model_input_dict=obs_dict)
+            #print("prev_action", info["observations"]["previous_action"])
+            #print("action:", action)
+
             _, _ , test_prob = agent(model_input_dict=obs_dict, action=action)
-            assert(log_prob == test_prob)
-            print(log_prob)
+            assert((log_prob == test_prob).all())
+            #print(log_prob)
             # rescale action to be between -1 and 1, 0.05 is one
             #action = action/0.05
             eval_env.render()
--- ftg_agents/agents_torch_wrapper.py
+++ ftg_agents/agents_torch_wrapper.py
@@ -1,1 +1,20 @@
-from ftg_agents.agents_numpy import 
+from ftg_agents.agents_numpy import StochasticContinousFTGAgent
+import torch
+class StochasticContinousFTGTorchWrapper(StochasticContinousFTGAgent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def __call__(self, obs, action=None, std=None):
+        # convert obs from torch to numpy, obs is a dictionary containing torch arrays
+        print(obs)
+        # loop over obs keys and transform to numpy from torch
+        np_obs = {}
+        print(obs)
+        for key in obs.keys():
+            np_obs[key] = obs[key].detach().numpy()
+        
+        _, action, log_probs = super().__call__(np_obs, action=action, std=None)
+        # convert action from numpy to torch
+        action = torch.from_numpy(action)
+        # convert log_probs from numpy to torch
+        log_probs = torch.from_numpy(log_probs)
+        return None, action, log_probs
